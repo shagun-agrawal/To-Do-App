083081690fe9d291f13c0452a695a092b7544ab2
shagun-agrawal <52834394+shagun-agrawal@users.noreply.github.com>
2021-04-08 14:29:50 +0530

    Add files via upload

---
+++ To-D0-App-main/base/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+
+from .models import Task
+
+# Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name='base/login.html'
+    fiels='__all__'
+    redirect_auhenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name='Task'
+    success_url = reverse_lazy('tasks')
+
